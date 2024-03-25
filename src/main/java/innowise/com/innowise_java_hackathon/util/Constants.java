3cdb8640236468aa909812b563f243b38b1ee1d6
Grigoriy Kislyi <RyhorKisly@gmail.com>
2024-03-25 03:54:17 +0300

    refactor util

---
+++ src/main/java/innowise/com/innowise_java_hackathon/util/Constants.java
@@ -0,0 +1,12 @@
+package innowise.com.innowise_java_hackathon.util;
+
+public class Constants {
+
+    public static final String START_DESCRIPTION = "Starts the bot";
+    public static final String START_TEXT = "Welcome to Innowise crypto Bot.\nPlease enter your name";
+    public static final String GREETING_MESSAGE = """
+        This bot is for tracking changes in exchange rates.
+        We monitor rate changes every 20 seconds. If during these 20 seconds there was a change in the exchange rate, you will know about it!
+        First, you need to set a threshold at which you will receive notifications about changes in exchange rates.
+        """;
+}

18c71714ba595befda9a9041106f00ad5629d96a
Grigoriy Kislyi <RyhorKisly@gmail.com>
2024-03-25 03:54:29 +0300

    refactor properties

--- src/main/java/innowise/com/innowise_java_hackathon/InnowiseJavaHackathonApplication.java
+++ src/main/java/innowise/com/innowise_java_hackathon/InnowiseJavaHackathonApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -20,9 +21,12 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class InnowiseJavaHackathonApplication {
 
 	public static void main(String[] args) throws TelegramApiException {
-		ConfigurableApplicationContext run = SpringApplication.run(InnowiseJavaHackathonApplication.class, args);
-		TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-		botsApi.registerBot(run.getBean("telegramBot", TelegramLongPollingBot.class));
+		ConfigurableApplicationContext ctx = SpringApplication.run(InnowiseJavaHackathonApplication.class, args);
+		try {
+			TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+			botsApi.registerBot(ctx.getBean("innowiseCryptocurrencyBot", AbilityBot.class));
+		} catch (TelegramApiException e) {
+			throw new RuntimeException(e);
+		}
 	}
-
 }
